79dbdcbe997e89eb3f290a40bad331b75a15ec68
MukeshKummuru <mukesh.kummuru@gmail.com>
2025-02-28 22:55:09 +0530

    after deleting new push

--- frontend/src/pages/WishlistPage.js
+++ frontend/src/pages/WishlistPage.js
@@ -13,7 +13,7 @@ const Wishlist = () => {
 
   return (
     <div className="wishlist-container">
-      <h2 className="wishlist-title-new">Your Wishlist</h2>
+      <h2 className="wishlist-title-new">YOUR WISHLIST</h2>
       {wishlist.length === 0 ? (
         <p className="empty-message">Your wishlist is empty</p>
       ) : (
